51cfb15d9cf09aeb8c65a8566082f837c3234919
Ed Jones <edjones@openhsitoryproject.org>
2011-02-22 15:44:53 -0500

    Initial committ - bare rails app
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whendidji3a::Application.config.secret_token = 'dca7da063300c654ebd3ba527279045785a521a6d371ed9bdc425b2822f53f60fa0a60000d5e6149c65d48e79c52dd0f8e7c0c620376c8106f97cf9c23cd27ea'

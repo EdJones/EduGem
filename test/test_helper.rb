c530632486fd15b8c384df712f9309b1fb21c6f1
Ed Jones <edjones@openhsitoryproject.org>
2011-06-21 13:14:09 -0400

    test helper mods
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# Add this to load Capybara integration:
+require 'capybara/rspec'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -10,4 +13,14 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+#Per http://trevorturk.com/2010/12/22/using-capybara-without-cucumber-in-rails-3/
+#setup do
+ #   Capybara.reset_sessions!
+  #end
 end
+
+#class ActionController::IntegrationTest
+#  include Capybara
+#end
+#end

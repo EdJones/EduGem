ec0aea5ac052a1ebbc73b79e79209f82477466f5
Ed Jones <edjones@openhsitoryproject.org>
2011-08-15 14:46:11 -0400

    And refactor out before(:each)
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

--- spec/controllers/my_digis_controller_spec.rb
+++ spec/controllers/my_digis_controller_spec.rb
@@ -11,36 +11,24 @@ render_views
 	 controller.stub!(:logged_in?).and_return(:current_account)
 	 session[:account_id] = @account.id	 
 	 controller.stub!(:login_required)
-
  end
 
   describe "GET index" do
     it "says 'Your Custom Event groups'" do
-
       get :index
       response.body.should =~ /(didjis)/m
     end
   end
 
   describe "GET my_digis/66/" do
-    it "says 'Deism'" do
-	 @account = Account.where(:username => 'ej0c').first
-	 controller.stub!(:current_account).and_return(@account)
-	 controller.stub!(:logged_in?).and_return(:current_account)
-	 session[:account_id] = @account.id	 
-	 controller.stub!(:login_required)
+    it "Previews the MyDigi'" do
       get :show, :id=> '66'
       response.body.should =~ /How many can you get?/m
     end
   end
   
     describe "GET my_digis/66/custom_events" do
-    it "says 'Events for my_digi #66'" do
-	 @account = Account.where(:username => 'ej0c').first
-	 controller.stub!(:current_account).and_return(@account)
-	 controller.stub!(:logged_in?).and_return(:current_account)
-	 session[:account_id] = @account.id	 
-	 controller.stub!(:login_required)
+    it "Lets you edit the MyDigi" do
       get "custom_events", :id=> '66'
       response.body.should =~ /Events for my_digi #66/m
     end
@@ -49,13 +37,6 @@ render_views
 #describe "View event details" do
 describe "GET my_digis/66/custom_events" do
   it "Should show a table of events" do
-	 @account = Account.where(:username => 'ej0c').first
-	 puts @account.inspect
-	 controller.stub!(:current_account).and_return(@account)
-	 controller.stub!(:logged_in?).and_return(:current_account)
-	 session[:account_id] = @account.id	 
-	 controller.stub!(:login_required)
-
      #visit '/my_digis/66/custom_events'
 	 visit custom_events_my_digi_path('66')
      page.should have_content('Events')

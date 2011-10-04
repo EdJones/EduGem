90f10417a7db6872d8a3f350787b4ffa0606261b
Ed Jones <edjones@openhsitoryproject.org>
2011-10-04 14:31:14 -0400

    No changes?
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
-
+ 
+    
+  private  
+    
+  def current_user  
+ #logger.debug "called current user  in AppController "    
+ #@current_user ||= Account.find(session[:username]) if session[:username] 
+ end  
+  
+    
+  #helper_method :current_user 
+helper_method :current_account 
 end

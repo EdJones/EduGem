34f4ec0665fc2e2db375866d72278a2a0bb71147
Ed Jones <edjones@openhsitoryproject.org>
2011-10-05 06:49:28 -0400

    Schema as working 1 Oct 2011
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110426123906) do
+ActiveRecord::Schema.define(:version => 20110913170335) do
 
   create_table "accounts", :force => true do |t|
     t.string   "username"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(:version => 20110426123906) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "invitation_limit"
+    t.integer  "invitation_id"
+    t.boolean  "subscribed"
+    t.boolean  "teacher"
+    t.boolean  "author"
+    t.boolean  "admin"
   end
 
   create_table "answers", :force => true do |t|

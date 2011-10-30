a7ae857b82d59279853f9e19156a283099bec7ea
Ed Jones <edjones@openhsitoryproject.org>
2011-10-30 18:24:53 -0400

    Fix 2 specs (ammend last)
    Signed-off-by: Ed Jones <edjones@openhsitoryproject.org>

--- spec/requests/my_games_spec.rb
+++ spec/requests/my_games_spec.rb
@@ -3,7 +3,6 @@ require "selenium-webdriver"
 
 describe "MyGames" do
   before :each do
-    #User.make(:email => 'user@example.com', :password => 'caplin')
 	@account = Account.where(:username => 'common_user').first
 	visit login_path
       fill_in 'Username or Email Address', :with => @account.email
@@ -12,11 +11,30 @@ describe "MyGames" do
   end
   
     it "Shows list of games" do
+	Factory
       visit my_games_path
 	  page.should have_content('My Custom Games')
+	  page.should have_content('Great Awakening')
     end
 
+    it "Gives you a form for a new game" do
+
+      visit my_games_path
+	  click_button('Create a New Game')
+	  page.should have_content('Start Creating Your Own Game')
+    end
+	
+    it "Lets you create a new game" do
+	new_game = Factory.build(:my_game, :author => "common_user", :title => "Knights Templar")
+      visit my_games_path
+	  click_button('Create a New Game')
+	  fill_in 'Title', :with => new_game.title
+	  click_button('Create') 
+	  page.should have_content(new_game.title)
+    end	
+
     it "Lets you modify a game" do
+	my_game = Factory.create(:my_game, :author => "common_user", :title => "Knights Templar")
       visit my_games_path
 	  click_button('Add/Edit Didjis')
 	  page.should have_content('My Custom Game')
